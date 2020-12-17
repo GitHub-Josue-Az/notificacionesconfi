51e7ee24e670cc4e6a0bdd2bd14b46b0149430d7
Josue Alva Zarate <josue.alva@tecsup.edu.pe>
2020-12-16 23:10:27 -0500

    notifi

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
 
         /* Funciona */      
-        $schedule->command('usuarios:cumples')->timezone('America/Lima')->at('21:04');
+        $schedule->command('usuarios:cumples')->timezone('America/Lima')->at('23:12');
 
          
     }
