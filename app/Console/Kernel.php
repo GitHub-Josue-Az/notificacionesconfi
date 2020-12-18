4e648e205168f8859aa0fa48d7a1bcde3b4cba59
Josue Alva Zarate <josue.alva@tecsup.edu.pe>
2020-12-17 21:08:46 -0500

    seconf

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
 
         /* Funciona */      
-        $schedule->command('usuarios:cumples')->timezone('America/Lima')->at('21:05');
+        $schedule->command('usuarios:cumples')->timezone('America/Lima')->at('21:10');
 
          
     }
