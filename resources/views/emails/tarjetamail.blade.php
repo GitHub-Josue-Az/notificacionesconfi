3cff1f884baa65b69250893fea9f0220fa9838e7
Josue Alva Zarate <josue.alva@tecsup.edu.pe>
2020-12-26 21:34:05 -0500

    jaja

--- resources/views/emails/tarjetamail.blade.php
+++ resources/views/emails/tarjetamail.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 	
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <body>
 
@@ -13,7 +12,7 @@
          <div>
          	
          	<img  src="{{route('confe.image',[31]) }}" width="130px" height="120px"><br>
-		<a class="btn-primary" style="background-color: green; box-shadow: 0 5px 0 darkgreen; color: white; padding: 14px 25px; text-decoration: none; text-transform: uppercase; display: inline-block;" href="{{route('conference.download',[31]) }}"> Descargar</a>
+		<a class="btn-primary" style="background-color: green; box-shadow: 0 5px 0 darkgreen; color: white; padding: 8px 15px; text-decoration: none; text-transform: uppercase; display: inline-block;" href="{{route('conference.download',[31]) }}"> Descargar</a>
 
 
          </div>             
