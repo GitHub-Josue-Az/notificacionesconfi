84575a2ce42d8da3afb659c648856ee9612088e0
Josue Alva Zarate <josue.alva@tecsup.edu.pe>
2020-12-28 02:54:57 -0500

    ultimos cambios 3

--- resources/views/emails/tarjetamail.blade.php
+++ resources/views/emails/tarjetamail.blade.php
@@ -12,9 +12,19 @@
          <div>
          	
             @foreach ($usuario->imagecumple as $imagenes)
+
+              <div class="row">
+               <div class="col-12 col-lg-6">
+                   <div class="card mb-4">
+                        <div class="card-body">
             
  <img  src="{{route('tarjeta.image',[$imagenes->id]) }}" width="130px" height="120px"><br>
  <a class="btn-primary" style="background-color: green; box-shadow: 0 5px 0 darkgreen; color: white; padding: 8px 15px; text-decoration: none; text-transform: uppercase; display: inline-block;" href="{{route('tarjeta.download',[$imagenes->id]) }}"> Descargar</a>
+
+                 </div>
+                    </div>
+                </div>
+             </div>
 
             @endforeach
 
--- resources/views/felicitadores/edit.blade.php
+++ resources/views/felicitadores/edit.blade.php
@@ -24,8 +24,7 @@
 
              <div class="row">
                <div class="col-12 col-lg-6">
-              
-                <div class="card mb-4">
+                   <div class="card mb-4">
                         <div class="card-body">
 
                       <div class="form-group ">
@@ -43,9 +42,7 @@
                             
                         </div>
                     </div>
-
                 </div>
-              
              </div>
 
 
